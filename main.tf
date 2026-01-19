4012730becc3a8ea12cacc955a1214ffd989a3bf
Rohan0322 <rohankumarna.11@gmail.com>
2026-01-19 02:22:33 -0800

    adding main.tf file to github

---
+++ main.tf
@@ -0,0 +1,34 @@
+module "m1" {
+  source  = "./myfolder"
+  file1   = var.mf
+  content = var.mc
+}
+
+module "m2" {
+  source  = "./myfolder"
+  file1    = "mfile2"
+  content = "Content from m2"
+}
+
+module "m3"{
+	source = "/home/rohan/Terraform/Modules/myfolder"
+	file1 = "mfile3"
+	content = "content from module 3 Absolute path" 
+}
+
+module "m4" {
+	source = "github.com/Rohan0322/Terraform.git"
+}
+
+variable "mf" {
+  default = "mfile1"
+}
+
+variable "mc" {
+  default = "file created from module 1"
+}
+
+output mo1{
+	value = module.m1.o1
+}
+
